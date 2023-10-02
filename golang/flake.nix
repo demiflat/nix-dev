bfdc6515d80779e347597e52565e00f9a72423f4
Darrell Kundel <darrell@demiflat.com>
2023-10-01 22:22:01 -0700

    genesis

---
+++ golang/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A Nix-flake-based Go development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      goVersion = 20; # Change this to update the whole stack
+      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
+      supportedSystems = [ "x86_64-linux" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # go 1.20 (specified by overlay)
+            go
+
+            # goimports, godoc, etc.
+            gotools
+
+            # https://github.com/golangci/golangci-lint
+            golangci-lint
+
+            # vscodium go plugin deps
+            go-outline
+            gopls
+            delve
+            gomodifytags
+            gotests
+            go-tools
+
+            #delve gui
+            gdlv
+          ];
+        };
+      });
+    };
+}
